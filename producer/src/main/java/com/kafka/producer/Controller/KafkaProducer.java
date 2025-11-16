5e8cf6c49b2cd600e0a814ec275f5fce0fa599b5
Harshalszz <477harshz@gmail.com>
2025-11-16 19:05:23 +0530

    now sending a Object from producer to consumer

--- producer/src/main/java/com/kafka/producer/Controller/KafkaProducer.java
+++ producer/src/main/java/com/kafka/producer/Controller/KafkaProducer.java
@@ -1,5 +1,6 @@
 package com.kafka.producer.Controller;
 
+import com.kafka.producer.RiderLocation;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class KafkaProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+//
+//    @PostMapping("/send")
+//    public String sendMessage(@RequestParam String message){
+//        kafkaTemplate.send("my-topic" , message);
+//        return "Messsage Sent : " + message;
+//    }
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    private final KafkaTemplate<String , RiderLocation> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, RiderLocation> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String message){
-        kafkaTemplate.send("my-topic" , message);
-        return "Messsage Sent : " + message;
+
+        RiderLocation riderLocation = new RiderLocation("rider1", 67.3,89.2);
+
+        kafkaTemplate.send("my-topic-new", riderLocation);
+
+        return "Location Send of rider : "+ riderLocation.getRiderId();
     }
 }
