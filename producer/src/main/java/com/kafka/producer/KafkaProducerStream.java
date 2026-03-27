4436518877d184f7bb379112a06ce2a279ded348
Harshalszz <477harshz@gmail.com>
2026-03-27 08:09:14 +0530

    removed

--- producer/src/main/java/com/kafka/producer/KafkaProducerStream.java
+++ producer/src/main/java/com/kafka/producer/KafkaProducerStream.java
@@ -29,6 +29,7 @@ public class KafkaProducerStream {
             RiderLocation riderLocation = new RiderLocation("XZ" , 23.34,56.353);
             System.out.println("Sending: " + riderLocation.getRiderId());
             System.out.println("from RiderLocation");
+            System.out.println("from RiderLocation");
             return riderLocation;
         };
     }
