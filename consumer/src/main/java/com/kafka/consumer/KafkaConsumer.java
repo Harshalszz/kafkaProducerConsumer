3eb34a4b13c8bc66336b8bdbaf830dbf0ba0e9c5
Harshalszz <477harshz@gmail.com>
2026-04-09 21:06:12 +0530

    we kafakListener

--- consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -21,6 +21,6 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "my-topic-new-1" , groupId = "my-new-group-1")
     public void listenRiderLocation(RiderLocation riderLocation){
-        System.out.println("Recevied Location 2: " + riderLocation.getRiderId());
+        System.out.println(" Location 2: " + riderLocation.getRiderId());
     }
 }
