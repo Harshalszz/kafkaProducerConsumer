ad9fa95db09eb53e19695e54d9bbde57e2287d9e
Harshalszz <477harshz@gmail.com>
2026-05-02 19:13:17 +0530

    kafak

--- consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -8,7 +8,7 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "my-topic" , groupId = "my-new-group")
     public void listen1(String message){
-        System.out.println("reciever  56: " + message);
+        System.out.println("reciever added 56: " + message);
     }
 
 
