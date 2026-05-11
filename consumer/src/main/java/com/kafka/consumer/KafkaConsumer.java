a3bd1c056cfd0fc9a02a70713c14409fcf7e20e2
Harshalszz <477harshz@gmail.com>
2026-05-11 21:18:42 +0530

    added topic

--- consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -21,6 +21,6 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "my-topic-new-1" , groupId = "my-new-group-1")
     public void listenRiderLocation(RiderLocation riderLocation){
-        System.out.println(" Location 2: " + riderLocation.getRiderId());
+        System.out.println(" adde 2: " + riderLocation.getRiderId());
     }
 }
