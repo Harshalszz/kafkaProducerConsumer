6f236926560497a0f31d386567b7f5aabec58014
Harshalszz <477harshz@gmail.com>
2026-05-26 21:09:17 +0530

    topic ki

--- consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -8,7 +8,7 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "my-topic" , groupId = "my-new-group")
     public void listen1(String message){
-        System.out.println(" r ece vi er : " + message);
+        System.out.println(" r: " + message);
     }
 
 
